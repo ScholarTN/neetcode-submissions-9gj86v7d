11e550d37f1f63ef3d30f9bde0d4de5026fe7092
THUBALAMI NKOMAZANA <thubalaminkomazana15@gmail.com>
2026-05-28 23:01:33 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,52 @@
+class TrieNode:
+
+    def __init__(self):
+
+        self.children = {}
+        self.end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+
+        node = self.root
+
+        for char in word:
+
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.end = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(index, node):
+
+            if index == len(word):
+                return node.end
+
+            char = word[index]
+
+            if char == ".":
+
+                for child in node.children.values():
+
+                    if dfs(index + 1, child):
+                        return True
+
+                return False
+
+            if char not in node.children:
+                return False
+
+            return dfs(index + 1,
+                       node.children[char])
+
+        return dfs(0, self.root)
