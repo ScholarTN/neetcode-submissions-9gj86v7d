4d1aa1235215849744ee06e3322b7ced38664678
THUBALAMI NKOMAZANA <thubalaminkomazana15@gmail.com>
2026-07-04 04:20:58 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public int canCompleteCircuit(int[] gas,
+                                  int[] cost) {
+
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for (int g : gas) {
+            totalGas += g;
+        }
+
+        for (int c : cost) {
+            totalCost += c;
+        }
+
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        int start = 0;
+        int tank = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return start;
+    }
+}
